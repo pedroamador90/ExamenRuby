4d9d2522e39c31961c6144eae7b1a2b8c0b50b3f
pedro <pedro.amador1090@gmail.com>
2016-04-01 00:17:05 -0600

    otro intento

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def batman
   end
-  
+
   def superman
+  end
+  def batman-vs-superman
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/batman'
   get 'pages/superman'
+  get 'pages/batman-vs-superman'
 
   root 'pages#batman-vs-superman'
 
