171b8e13bde434cb3bf79aef63bbba92f0160464
pedro <pedro.amador1090@gmail.com>
2016-04-01 01:19:57 -0600

    intento

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+=begin
+  def index
+  	@votos =[params :heroe, :email]
+  end
+=end
   def batman
   end
 
   def superman
   end
   def batman_vs_superman
+  	@votos =[params :heroe, :email]
   end
 end
