d2af13fe639d91bc6e9f694115b206dd8856834a
pedro <pedro.amador1090@gmail.com>
2016-04-01 01:58:04 -0600

    ya medio jala

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,17 @@ class PagesController < ApplicationController
 
   def superman
   end
+
   def batman_vs_superman
-  	@votos =[params :heroe, :email]
+  end
+
+   def batman_wins
+  	Voto.create(heroe: 'batman',email: params[:batman_wins])
+  	redirect_to pages_batman_path, notice: "Voto Guardado"
+  end
+
+  def superman_wins
+  	Voto.create(heroe: 'superman',email: params[:superman_wins])
+  	redirect_to pages_superman_path, notice: "Voto Guardado"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'pages/superman'
   get 'pages/batman_vs_superman'
 
+  post 'pages/batman_wins'
+  post 'pages/superman_wins'
+
   root 'pages#batman_vs_superman'
 
   end
